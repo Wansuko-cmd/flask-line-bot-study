33fa0d0f9f900bfc9ddcd52ddd813e1fdf0fbfee
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2021-06-18 13:59:32 +0900

    [add] configの設定

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask
+from linebot import LineBotApi
 
 app = Flask(__name__)
+app.config.from_object('config.LineToken')
+
+# line_bot_api = LineBotApi('')
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return app.config['CHANNEL_ACCESS_TOKEN']
 
 
 if __name__ == '__main__':
